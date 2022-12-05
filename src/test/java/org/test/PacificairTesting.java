69b0674d2b2461f80840c79f48110fef514a9b4a
Sujatha <117371998+Suja0711@users.noreply.github.com>
2022-12-05 17:50:13 +0530

    Update PacificairTesting.java

--- src/test/java/org/test/PacificairTesting.java
+++ src/test/java/org/test/PacificairTesting.java
@@ -1,48 +1,23 @@
 package org.test;
 
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-
-import org.data.DataCollection;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.net.EphemeralPortRangeDetector;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.pages.ManageCheckIn;
-import org.pages.ManageLoginPage;
+
+import org.pages.BookingPage;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.utility.BaseClass;
 
 
-public class PacificairTesting extends BaseClass{
-	ManageLoginPage mg;
-	ManageCheckIn chkObj;
-	WebDriverWait wt;
-	Actions as;
-	
+
+public class PacificairTesting extends BaseClass {
+	static BookingPage bk;
 	@BeforeClass
 	public void beforeClass() {
-		//https://www.cebupacificair.com/
+		
 		System.out.println("Before class method");
-		browserLaunch("chrome");
-		urlLaunch("https://www.cebupacificair.com/");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
-		wt = new WebDriverWait(driver, 5000);
-		mg = new ManageLoginPage();
-		as = new Actions(driver);
 	}
 	
 	@AfterClass
@@ -54,44 +29,15 @@ public class PacificairTesting extends BaseClass{
 	private void beforeMethod() {
 		System.out.println("Before method");
 	}
+	
 	@AfterMethod
-	private void afterMethod() throws InterruptedException {
+	private void afterMethod() {
 		System.out.println("After method");
 	}
 	
-	@Test(dataProvider="loginModule", dataProviderClass=DataCollection.class,priority=-4)
-	private void loginMethod(String str1, String loginLnkXpath, String loginBtnXpath, String subLnkXpath, String email, String pass) {
-		System.out.println("Login Method ------------");
-		mg.clickLoginBtn(str1,loginLnkXpath,loginBtnXpath,subLnkXpath,email,pass);
-	}
-	
-	@Test(dataProvider="chkInModule",dataProviderClass=DataCollection.class,priority=-3)
-	private void manageChkModule(String chkInXpath, String email, String pass, String url) {
-		chkObj = new ManageCheckIn();
-		System.out.println("Manage Module checkin ------------");
-		as.moveToElement(chkObj.getMngLnk()).click(chkObj.getCheckEle()).perform();
-		//wt.until(ExpectedConditions.visibilityOf(chkObj.getEmailChk()));
-		sendKeys(chkObj.getEmailChk(), email);
-		sendKeys(chkObj.getPassChk(), pass);
-		clickBtn(chkObj.getLoginChk());
-		driver.navigate().to(url);
-	}
-	
-	@Test(enabled=true,dataProvider="urlModule",dataProviderClass=DataCollection.class,priority=-2)
-	private void manageBookModule(String url,String url1) throws InterruptedException {
-		System.out.println("Manage Module Booking ------------");
-		//wt.until(ExpectedConditions.visibilityOf(chkObj.getMngBookEle()));
-		as.moveToElement(chkObj.getMngLnk()).click(chkObj.getMngBookEle()).perform();
-		Thread.sleep(2000);
-		driver.navigate().to(url);
-	}
-	
-	@Test(enabled=true,dataProvider="urlModule",dataProviderClass=DataCollection.class,priority=-1)
-	private void manageFlightStatus(String url,String url1) throws InterruptedException {
-		System.out.println("Manage Module Flight Status ------------");
-		as.moveToElement(chkObj.getMngLnk()).click(chkObj.getMngFltStatus()).perform();
-		Thread.sleep(2000);
-		driver.navigate().to(url);		
+	@Test(enabled=true)
+	private void Book1() {
+		
 	}
 
 }
